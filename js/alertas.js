d7988720d5f3928da480b75ebabbec077c77310f
SandroMassi <sandro.massi@uab.edu.bo>
2023-03-12 19:05:19 -0400

    Actualizado boton

--- js/alertas.js
+++ js/alertas.js
@@ -3,10 +3,8 @@ import swal from 'sweetalert';
 function mostrar(){
     swal("" ,{ 
         title:'¿ Quieres volver a jugar ?',
-        buttons: [" OK DE ACUERDO ", "YA NO"]
+        buttons: [" si ", " NO "]
         });
 }
-swal.fire({
-    title: "Bien venido"
-});
+
 
